ef2c8c81241fa59c040d8c98de0eeb630eeda4bc
Jenny Shih <jinghua.shih@gmail.com>
2022-12-05 22:10:54 -0500

    Complete day 5

--- day5.rb
+++ day5.rb
@@ -7,21 +7,32 @@ Procedure = Data.define(:num, :from, :to)
 class Day5 < Runner
   def do_puzzle1
     raw_form, raw_procedure = @input.split("\n\n")
+
+    load_cargos = ->(stack, memo) {
+      stack_num = stack.pop.to_i
+      trimmed_stack = stack.reject { |cargo| cargo == ' ' }.reverse
+      memo[stack_num] = trimmed_stack
+    }
+
     form = raw_form
            .split("\n")
            .map { _1.split('') }
            .transpose
            .select { |line| line.any? { |elem| elem =~ /\w/ } }
-           .each_with_object({}) do |stack, memo|
-      memo[stack.pop.to_i] = stack.reject { |cargo| cargo == ' ' }.reverse
-    end
+           .each_with_object({}, &load_cargos)
 
-    procedures = raw_procedure.split("\n").map do
-      Procedure.new(*(_1.split(' ').map(&:to_i).select do |elem|
-                        !elem.zero?
-                      end))
-    end
-    procedures.each do |procedure|
+    extract_numbers = ->(line) {
+      line
+        .split(' ')
+        .map(&:to_i)
+        .reject(&:zero?)
+    }
+
+    raw_procedure
+      .split("\n")
+      .map(&extract_numbers)
+      .map { Procedure.new(*_1) }
+      .each do |procedure|
       move = form[procedure.from].pop(procedure.num).reverse
       form[procedure.to].concat(move)
     end
@@ -31,21 +42,32 @@ class Day5 < Runner
 
   def do_puzzle2
     raw_form, raw_procedure = @input.split("\n\n")
+
+    load_cargos = ->(stack, memo) {
+      stack_num = stack.pop.to_i
+      trimmed_stack = stack.reject { |cargo| cargo == ' ' }.reverse
+      memo[stack_num] = trimmed_stack
+    }
+
     form = raw_form
            .split("\n")
            .map { _1.split('') }
            .transpose
            .select { |line| line.any? { |elem| elem =~ /\w/ } }
-           .each_with_object({}) do |stack, memo|
-      memo[stack.pop.to_i] = stack.reject { |cargo| cargo == ' ' }.reverse
-    end
+           .each_with_object({}, &load_cargos)
 
-    procedures = raw_procedure.split("\n").map do
-      Procedure.new(*(_1.split(' ').map(&:to_i).select do |elem|
-                        !elem.zero?
-                      end))
-    end
-    procedures.each do |procedure|
+    extract_numbers = ->(line) {
+      line
+        .split(' ')
+        .map(&:to_i)
+        .reject(&:zero?)
+    }
+
+    raw_procedure
+      .split("\n")
+      .map(&extract_numbers)
+      .map { Procedure.new(*_1) }
+      .each do |procedure|
       move = form[procedure.from].pop(procedure.num)
       form[procedure.to].concat(move)
     end
